76bd404e2754a0c8e35b0a917ea149d1371218eb
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2017-01-11 17:03:18 -0800

    first commit

---
+++ grails-app/init/BootStrap.groovy
@@ -0,0 +1,23 @@
+import com.ociweb.*
+
+    class BootStrap {
+
+        def init = { servletContext ->
+
+            if(!Todo.list()) {
+                log.info "Creating todos..."
+                def todoList = new TodoList(name: "Bob's List").save()
+
+                [[name: "Task 1", todoList: todoList],
+                 [name: "Task 2", todoList: todoList],
+                 [name: "Task 3", todoList: todoList]].each { props ->
+                    def todo = new Todo()
+                    todo.properties = props
+                    todo.save(flush: true)
+                }
+
+            }
+        }
+        def destroy = {
+        }
+    }
